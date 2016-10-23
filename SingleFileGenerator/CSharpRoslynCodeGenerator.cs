0f24e519a8cbcea4e9337d03d30d76cef26d15c0
Alphaleonis <alphaleonis@users.noreply.github.com>
2016-10-23 19:04:43 +0200

    Added SingleFileGenerator utilities from AlphaVSX.

---
+++ SingleFileGenerator/CSharpRoslynCodeGenerator.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Simplification;
+using System;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Runtime.InteropServices;
+using Microsoft.CodeAnalysis.Formatting;
+
+namespace Alphaleonis.Vsx
+{
+   [ComVisible(true)]
+   public abstract class CSharpRoslynCodeGenerator : RoslynCodeGeneratorBase
+   {
+      protected sealed override async Task<Document> GenerateCodeAsync(Document inputDocument)
+      {         
+         ReportProgress(0, 100);         
+         CompilationUnitSyntax compilationUnit = await GenerateCompilationUnit(inputDocument);
+         SyntaxNode targetNode = Formatter.Format(compilationUnit, inputDocument.Project.Solution.Workspace);
+         Document resultDocument = inputDocument.Project.AddDocument(Guid.NewGuid().ToString() + ".tmp.g.cs", targetNode);
+         resultDocument = await Simplifier.ReduceAsync(resultDocument);
+         ReportProgress(100, 100);
+         return resultDocument;
+      }
+
+      protected abstract Task<CompilationUnitSyntax> GenerateCompilationUnit(Document sourceDocument);
+   }
+}
